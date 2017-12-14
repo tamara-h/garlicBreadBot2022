8f6cf8ae10a6450ad33b2b6962cbd7e45418e7ae
tamara-h <mixedsoups@gmail.com>
2017-12-14 16:01:36 +0000

    finally get everything working!!

--- bot.js
+++ bot.js
@@ -9,34 +9,41 @@ let Twitter = new twit(config);
 // find latest tweet according the query 'q' in params
 let retweet = function() {
     const params = {
-        q: '#garlicBread, #garlic, #bread, #pizza',  // REQUIRED
-        result_type: 'recent',
+        q: '#garlicBread',  // REQUIRED
+        result_type: 'mixed',
         lang: 'en'
     };
     Twitter.get('search/tweets', params, function(err, data) {
         // if there no errors
         if (!err) {
-            // grab ID of tweet to retweet
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });
+
+            // find tweets
+            let tweet = data.statuses;
+            let randomTweet = ranDom(tweet);   // pick a random tweet
+
+            // if random tweet exists
+
+            if(typeof randomTweet !== 'undefined'){
+                // Tell TWITTER to retweet
+                Twitter.post('statuses/retweet/:id', {
+                    id: randomTweet.id_str
+                }, function(err, response) {
+                    if (response) {
+                        console.log('Retweeted!!!');
+                    }
+                    // if there was an error while tweeting
+                    if (err) {
+                        console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                    }
+                });
+            }
         }
         // if unable to Search a tweet
         else {
             console.log('Something went wrong while SEARCHING...');
         }
     });
-}
+};
 
 // grab & retweet as soon as program is running...
 retweet();
@@ -46,30 +53,40 @@ retweet();
 // find a random tweet and 'favorite' it
 let favoriteTweet = function(){
     const params = {
-        q: '#garlicBread, #garlic, #bread, #pizza',  // REQUIRED
+        q: '#garlicBread',  // REQUIRED
         result_type: 'recent',
         lang: 'en'
     };
     // find the tweet
     Twitter.get('search/tweets', params, function(err,data){
 
-        // find tweets
-        let tweet = data.statuses;
-        let randomTweet = ranDom(tweet);   // pick a random tweet
+        if(!err){
+            // find tweets
+            let tweet = data.statuses;
+            let randomTweet = ranDom(tweet);   // pick a random tweet
 
-        // if random tweet exists
-        if(typeof randomTweet !== 'undefined'){
-            // Tell TWITTER to 'favorite'
-            Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
-                // if there was an error while 'favorite'
-                if(err){
-                    console.log('CANNOT BE FAVORITE... Error');
-                }
-                else{
-                    console.log('FAVORITED... Success!!!');
-                }
-            });
+            // if random tweet exists
+            if(typeof randomTweet !== 'undefined'){
+                // Tell TWITTER to 'favorite'
+                Twitter.post('favorites/create', {
+                    id: randomTweet.id_str
+                }, function(err, response){
+                    // if there was an error while 'favorite'
+                    if(err){
+                        console.log('CANNOT BE FAVORITE... Error');
+                    }
+                    else{
+                        console.log('FAVORITED... Success!!!');
+                    }
+                });
+            }
         }
+        // if unable to Search a tweet
+        else {
+            console.log('Something went wrong while SEARCHING...');
+        }
+
+
     });
 };
 
