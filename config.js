fd487948cc7b10b2f0026c148db4d7d5abb8b099
tamara-h <mixedsoups@gmail.com>
2017-12-14 11:55:20 +0000

    create basic structure

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: '28UZ61lzKBCqRGrYSXTy9Cft6',
+    consumer_secret: '8nd6Q8XnrhSJvVrSL2KUYphT8gEavj9zTPsLCnyXp3WsfTjNeZ',
+    access_token: '934842438996328450-K4wo9ga6Z9VIh4Ig9fzJaGQMSoYMrqK',
+    access_token_secret: 'fsgszW7vrupyDgAJ5HY6fAzGy0UoyEgcDXUxQaHAQn8RI'
+};
