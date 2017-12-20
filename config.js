3d1d48d073e3cc5f74559dc349cca46761b029d0
tamara-h <mixedsoups@gmail.com>
2017-12-20 09:02:26 +0000

    remove old env variables

--- config.js
+++ config.js
@@ -7,17 +7,17 @@
  */
 
 
-// module.exports = {
-//     consumer_key: process.env.BOT_CONSUMER_KEY,
-//     consumer_secret: process.env.BOT_CONSUMER_SECRET,
-//     access_token: process.env.BOT_ACCESS_TOKEN,
-//     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-// };
-
 module.exports = {
-    consumer_key: '28UZ61lzKBCqRGrYSXTy9Cft6',
-    consumer_secret: '8nd6Q8XnrhSJvVrSL2KUYphT8gEavj9zTPsLCnyXp3WsfTjNeZ',
-    access_token: '934842438996328450-K4wo9ga6Z9VIh4Ig9fzJaGQMSoYMrqK',
-    access_token_secret: 'fsgszW7vrupyDgAJ5HY6fAzGy0UoyEgcDXUxQaHAQn8RI'
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 };
-
+//
+// module.exports = {
+//     consumer_key: '28UZ61lzKBCqRGrYSXTy9Cft6',
+//     consumer_secret: '8nd6Q8XnrhSJvVrSL2KUYphT8gEavj9zTPsLCnyXp3WsfTjNeZ',
+//     access_token: '934842438996328450-K4wo9ga6Z9VIh4Ig9fzJaGQMSoYMrqK',
+//     access_token_secret: 'fsgszW7vrupyDgAJ5HY6fAzGy0UoyEgcDXUxQaHAQn8RI'
+// };
+//
