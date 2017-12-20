c1777836057ec2e5fb5bd953844dcddffddea220
tamara-h <mixedsoups@gmail.com>
2017-12-20 09:06:22 +0000

    remove old api keys

--- config.js
+++ config.js
@@ -13,11 +13,3 @@ module.exports = {
     access_token: process.env.BOT_ACCESS_TOKEN,
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 };
-//
-// module.exports = {
-//     consumer_key: '28UZ61lzKBCqRGrYSXTy9Cft6',
-//     consumer_secret: '8nd6Q8XnrhSJvVrSL2KUYphT8gEavj9zTPsLCnyXp3WsfTjNeZ',
-//     access_token: '934842438996328450-K4wo9ga6Z9VIh4Ig9fzJaGQMSoYMrqK',
-//     access_token_secret: 'fsgszW7vrupyDgAJ5HY6fAzGy0UoyEgcDXUxQaHAQn8RI'
-// };
-//
